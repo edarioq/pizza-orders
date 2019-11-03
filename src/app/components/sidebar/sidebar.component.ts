ac831e596710498ff35a45fe5370704e2bd1f833
Edgar Quintero <edarioq@gmail.com>
2019-11-03 16:09:15 -0500

    Add sidebar layout

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.sass']
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+
+  private sidebar: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.sidebar = [
+      {
+        name: 'Dashboard',
+        icon: 'dashboard.svg',
+        active: false
+      },
+      {
+        name: 'New Order',
+        icon: 'new-order.svg',
+        active: true
+      },
+      {
+        name: 'Status',
+        icon: 'order-status.svg',
+        active: false
+      }
+    ]
   }
 
 }
